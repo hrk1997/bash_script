c5a613a03cee418a1824a1d848cf4ba2c53a8f54
hrk <hrk@localhost.localdomain>
2020-04-27 22:14:38 +0530

    new bashscript forr install google,audacity,simplescreenrecoder

---
+++ basic_needs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#terminator
+sudo apt-get install -y terminator
+
+#virtualbox
+sudo apt-get install -y virtualbox
+
+#audacity
+sudo apt-get install -y audacity
+
+#simple_screen_recoder
+sudo apt-get install -y simplescreenrecorder
